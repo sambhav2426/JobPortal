4f5106bfabbf3c912af5df2119e6615092e74d6f
Seema-25 <seema.kumari@coredge.io>
2026-04-14 00:15:37 +0530

    saved jobs

--- jobportal/src/main/java/com/project/jobportal/controller/JobSeekerApplyController.java
+++ jobportal/src/main/java/com/project/jobportal/controller/JobSeekerApplyController.java
@@ -1,16 +1,11 @@
 package com.project.jobportal.controller;
 
-import com.project.jobportal.entity.JobPostActivity;
-import com.project.jobportal.entity.JobSeekerApply;
-import com.project.jobportal.entity.JobSeekerProfile;
-import com.project.jobportal.entity.Users;
-import com.project.jobportal.services.JobPostActivityService;
-import com.project.jobportal.services.JobSeekerApplyService;
-import com.project.jobportal.services.JobSeekerProfileService;
-import com.project.jobportal.services.UsersService;
+import com.project.jobportal.entity.*;
+import com.project.jobportal.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -26,21 +22,59 @@ public class JobSeekerApplyController {
 
     private final JobPostActivityService jobPostActivityService;
     private final UsersService usersService;
-    private final JobSeekerProfileService jobSeekerProfileService;
     private final JobSeekerApplyService jobSeekerApplyService;
+    private final JobSeekerSaveService jobSeekerSaveService;
+    private final RecruiterProfileServices recruiterProfileServices;
+    private final JobSeekerProfileService jobSeekerProfileService;
 
     @Autowired
-    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService, JobSeekerProfileService jobSeekeProfileService, JobSeekerProfileService jobSeekerProfileService, JobSeekerApplyService jobSeekerApplyService) {
+    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService, JobSeekerProfileService jobSeekeProfileService, JobSeekerProfileService jobSeekerProfileService, JobSeekerApplyService jobSeekerApplyService, JobSeekerSaveService jobSeekerSaveService, RecruiterProfileServices recruiterProfileServices, JobSeekerProfileService jobSeekerProfileService1) {
         this.jobPostActivityService = jobPostActivityService;
         this.usersService = usersService;
-        this.jobSeekerProfileService = jobSeekerProfileService;
         this.jobSeekerApplyService = jobSeekerApplyService;
+        this.jobSeekerSaveService = jobSeekerSaveService;
+        this.recruiterProfileServices = recruiterProfileServices;
+        this.jobSeekerProfileService = jobSeekerProfileService1;
     }
 
     @GetMapping("job-details-apply/{id}")
     public String display(@PathVariable("id") int id, Model model) {
         JobPostActivity jobDetails = jobPostActivityService.getOne(id);
-        
+        List<JobSeekerApply> jobSeekerApplyList = jobSeekerApplyService.getJobCandidates(jobDetails);
+        List<JobSeekerSave> jobSeekerSaveList = jobSeekerSaveService.getJobCandidates(jobDetails);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)) {
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                RecruiterProfile user = recruiterProfileServices.getCurrentRecruiterProfile();
+                if(user != null) {
+                    model.addAttribute("applyList", jobSeekerApplyList);
+                }
+            } else {
+                JobSeekerProfile user = jobSeekerProfileService.getCurrentSeekerProfile();
+                if(user != null) {
+                    boolean exist = false;
+                    boolean saved = false;
+                    for(JobSeekerApply jobSeekerApply: jobSeekerApplyList) {
+                        if(jobSeekerApply.getUserId().getUserAccountId() == user.getUserAccountId()) {
+                            exist = true;
+                            break;
+                        }
+                    }
+                    for(JobSeekerSave jobSeekerSave: jobSeekerSaveList) {
+                        if(jobSeekerSave.getUserId().getUserAccountId() == user.getUserAccountId()) {
+                            saved = true;
+                            break;
+                        }
+                    }
+                    model.addAttribute("alreadyApplied", exist);
+                    model.addAttribute("alreadySaved", saved);
+                }
+            }
+        }
+        JobSeekerApply jobSeekerApply = new JobSeekerApply();
+        model.addAttribute("applyJob", jobSeekerApply);
+
         model.addAttribute("jobDetails", jobDetails);
         model.addAttribute("user", usersService.getCurrentUserProfile());
         return "job-details";
--- jobportal/src/main/java/com/project/jobportal/services/RecruiterProfileServices.java
+++ jobportal/src/main/java/com/project/jobportal/services/RecruiterProfileServices.java
@@ -1,8 +1,15 @@
 package com.project.jobportal.services;
 
 import com.project.jobportal.entity.RecruiterProfile;
+import com.project.jobportal.entity.Users;
 import com.project.jobportal.repository.RecruiterProfileRepository;
+import com.project.jobportal.repository.UsersRepository;
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,10 +18,12 @@ import java.util.Optional;
 public class RecruiterProfileServices {
 
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final UsersRepository usersRepository;
 
     @Autowired
-    public RecruiterProfileServices(RecruiterProfileRepository recruiterProfileRepository) {
+    public RecruiterProfileServices(RecruiterProfileRepository recruiterProfileRepository, UsersRepository usersRepository) {
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.usersRepository = usersRepository;
     }
 
     public Optional<RecruiterProfile> getOne(Integer id) {
@@ -23,5 +32,15 @@ public class RecruiterProfileServices {
 
     public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
         return recruiterProfileRepository.save(recruiterProfile);
+    }
+
+    public RecruiterProfile getCurrentRecruiterProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            Optional<RecruiterProfile> recruiterProfile = getOne(users.getUserId());
+            return recruiterProfile.orElse(null);
+        } else return null;
     }
 }
